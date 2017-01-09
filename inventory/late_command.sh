2d3d45f15d15ba7986d1193147b6115a694854a7
Stefano Rivera <stefano@rivera.za.net>
2017-01-10 08:24:13 +1100

    Get late_command from this repo

---
+++ inventory/late_command.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This script setups ansible and runs it
+# It should be ran at the end of the basic installation of a machine
+
+apt install -y ansible git
+
+# We clone our ansible repository and copy the ansible config files
+
+git clone https://anonscm.debian.org/git/debconf-video/ansible.git /root/debconf-ansible
+git clone https://github.com/xfxf/lca2017-av.git /root/lca2017-av
+
+# Aaaand we run ansible
+
+ansible-playbook --inventory-file=/root/lca2017-av/inventory/hosts \
+	--connection=local \
+	--limit=$(hostname) \
+	/root/debconf-ansible/site.yml
