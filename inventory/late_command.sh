710722b8714f77a090285fad914bf6742728f3e3
Stefano Rivera <stefano@rivera.za.net>
2017-01-13 16:40:17 +1100

    Unset http_proxy, to allow ansible to fetch files without sending everything through a squid-deb-proxy

--- inventory/late_command.sh
+++ inventory/late_command.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+set -euf
+
 # This script setups ansible and runs it
 # It should be ran at the end of the basic installation of a machine
 
 apt install -y ansible git eatmydata
+
+# d-i exports http_proxy, if it had a proxy to use
+unset http_proxy
 
 # We clone our ansible repository and copy the ansible config files
 
@@ -11,7 +16,6 @@ git clone https://anonscm.debian.org/git/debconf-video/ansible.git /root/debconf
 git clone https://github.com/xfxf/lca2017-av.git /root/lca2017-av
 
 # Aaaand we run ansible
-
 eatmydata ansible-playbook --inventory-file=/root/lca2017-av/inventory/hosts \
 	--connection=local \
 	--limit=$(hostname) \
